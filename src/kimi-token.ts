dd46a2f08672d812808bd972850fdddf143dcfd7
sublime247 <kushimobashir2000@gmail.com>
2024-10-05 09:59:04 +0100

    erc20token subgraph

--- src/kimi-token.ts
+++ src/kimi-token.ts
@@ -1,56 +1,19 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import { KimiToken, Approval, Transfer } from "../generated/KimiToken/KimiToken"
-import { ExampleEntity } from "../generated/schema"
+import { loadOrCreateUser } from "./utils";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+export function handleTransfer(event: Transfer): void {
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
+  let sender = loadOrCreateUser(event.transaction.from.toHexString());
+  let receiver = loadOrCreateUser(event.params.to.toHexString());
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  let amount = event.params.value;
+  sender.balance = sender.balance.minus(amount);
+  receiver.balance = receiver.balance.plus(amount);
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  sender.save();
+  receiver.save();
 
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.spender = event.params.spender
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allowance(...)
-  // - contract.approve(...)
-  // - contract.balanceOf(...)
-  // - contract.decimals(...)
-  // - contract.name(...)
-  // - contract.owner(...)
-  // - contract.symbol(...)
-  // - contract.totalSupply(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
 }
 
-export function handleTransfer(event: Transfer): void {}
+// export function handleTransfer(event: Transfer): void {}
